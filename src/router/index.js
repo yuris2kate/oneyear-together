327461c4b4d632f6c3ef3519d6a19fa11a9dae18
yuris2kate <dev.petersonmachado@gmail.com>
2025-05-31 02:36:09 -0300

    Testando

--- src/router/index.js
+++ src/router/index.js
@@ -3,40 +3,8 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/home',
-      name: 'home',
-      component: () => import('../views/Home.vue'),
-    },
-    {
-      path: '/warning',
-      name: 'warning',
-      component: () => import('../views/ScreenWarning.vue'),
-    },
-    {
       path: "/",
-      redirect: () => {
-        return localStorage.getItem("screenWarning") ? "/home" : "/warning";
-      }
-    },
-    {
-      path: '/momentos',
-      name: 'momentos',
       component: () => import('../views/Momentos.vue'),
-    },
-    {
-      path: '/whatsapp',
-      name: 'whatsapp',
-      component: () => import('../views/Whatsapp.vue'),
-    },
-    {
-      path: '/analytics',
-      name: 'analytics',
-      component: () => import('../views/AnalyticsPage.vue'),
-    },
-    {
-      path: '/enigma',
-      name: 'enigma',
-      component: () => import('../views/EnigmaPage.vue'),
     }
   ],
 })
